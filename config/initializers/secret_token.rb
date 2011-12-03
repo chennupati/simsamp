f4189df17e7ddfbc48573c0d7fb8679ce030d96f
Chennupati Gopinath <chennupati.1210@gmail.com>
2011-12-03 21:16:26 +0000

    simple

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simsamp::Application.config.secret_token = '52c6d9d69f9dc94f0ef805a5728aedca3f106595280c69c588d433fea2cdb34f62eafaacaab9e9ef04b88c33a37a07c7bc7a6a77899b2aa548c0843b1033ab50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Simsamp::Application.config.session_store :cookie_store, :key => '_simsamp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Simsamp::Application.config.session_store :active_record_store
